5b99be74ef2e7f46425d24797d430b2a8bb2a3e2
rg492 <91168366+rg492@users.noreply.github.com>
2023-04-04 21:24:21 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	private final static String title = "Faculty Recommendation Compiler";
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("LoginFX.fxml"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle(title);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
